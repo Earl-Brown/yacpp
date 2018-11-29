eb2ccc2f0c2e08599deec2d25156bccea2e3d4bc
Earl <Earl-Brown@users.noreply.github.com>
2018-11-28 21:50:35 -0700

    Create parser.js

---
+++ parser.js
@@ -0,0 +1,247 @@
+/*
+
+  todo:
+    * recognize aliases
+    * recognize closing tags
+		* Add attribute to indicate broken words
+
+*/
+
+import { hasValue, trimArray } from "./utilities.js"
+
+const reDirectiveSpec = /{([^:}]*)(:\s*?)?(.*?)}/gi,
+  reSplitSentenceToTerms = /(.*?)(\[.*?])/gi
+
+class EmptyLine {}
+
+class Directive {
+  constructor(source) {
+    if (!source) return
+
+    this.Source = source
+    let split = source.split(reDirectiveSpec).filter(hasValue)
+    ;[this.Type, , this.Detail] = split.filter(hasValue).map(x => x.toString().trim())
+  }
+}
+
+class Comment extends Directive {}
+
+class ChordInfo {
+  constructor(source) {
+    this.Source = source.replace(/\[?\]?/gi, "")
+    let a = this.Source.split(" ")
+
+    this.Symbol = a.shift() || ""
+    this.Comment = a.join(" ")
+  }
+}
+
+class Phrase {
+  constructor(chord, text) {
+    this.Chord = chord
+    this.Text = text
+  }
+}
+
+class Sentence {
+  constructor(source) {
+    source = source.toString()
+    this.Source = source
+    this.Phrases = []
+
+
+    var connectedChordsPlaceholder = "&nbsp;",
+      content = source
+      .replace(/\]\s*\[/gi, `]${connectedChordsPlaceholder}[`)
+      content = content.split(reSplitSentenceToTerms)
+      .map(x => x.trim())
+
+    // trim edges of array
+    // ensure chord is first token and text is last token
+    // convert tokens to phrases
+    // ensure proper pattern of chord,text,chord,text...
+    content = trimArray(content).filter(x => x != "")
+
+    if (content[0].indexOf("[") != 0) {
+      content.unshift("[]")
+    }
+
+    while (content.length > 0) {
+      var [chord, text] = content
+      if (chord === undefined) chord = ""
+      if (text === undefined) text = ""
+      if (text === connectedChordsPlaceholder) text = ""
+
+      this.Phrases.push(new Phrase(new ChordInfo(chord), text))
+      content.shift()
+      content.shift()
+    }
+  }
+}
+
+class Lyric extends Directive {
+  constructor(type) {
+    super()
+    this.Type = type
+    this.Sentences = []
+  }
+}
+
+class Song {
+  constructor() {
+    Object.assign(this, {
+      Title: "",
+      Artist: "",
+      Copyright: "",
+      Key: "",
+      License: "",
+
+      Comments: [],
+      Directives: []
+    })
+  }
+
+  FilterDirectivesByType(type) {
+    return this.Directives.filter(
+      p => p.Type.toLowerCase() == type.toLowerCase()
+    )
+  }
+
+  get Verses() {
+    return this.FilterDirectivesByType("verse")
+  }
+
+  get Chorii() {
+    return this.FillterDirectivesByType("chorus")
+  }
+
+  get Choruses() {
+    return this.Chorii
+  }
+}
+
+class SongParser {
+  constructor(songText) {
+    this.Source = songText
+    function splitToLines(songText) {
+      let split = songText.toString()
+      
+      
+      split = split.replace(/[\r\n]+/gim,"\\r")
+      split = split.split("\\r")
+
+			// split = split.replace(/[\r\n]/gim, "\r")
+			// split = split.split("\r")
+			split = split.map(l => l.trim())
+
+			return split
+    }
+
+    // determine type, return instance of types
+    //	* directive
+    //	* comment
+    //	* empty
+    //	* lyric
+    // return a class based on type
+    function preParseLine(line) {
+			line = line && line.trim()
+      if (!line || line === "") return new EmptyLine()
+      if (line.startsWith("#")) return new Comment(line)
+      if (line.startsWith("{")) return new Directive(line)
+      return new Sentence(line)
+    }
+
+    function parse() {
+      let song = new Song()
+      let currentDirective = null
+      let lines = splitToLines(songText).map(preParseLine)
+
+      function StartNextDirective(next) {
+        if (currentDirective) {
+          song.Directives.push(currentDirective)
+        }
+        currentDirective = next
+      }
+
+      while (lines.length > 0) {
+        var line = lines.shift()
+        switch (true) {
+          case line instanceof EmptyLine:
+            StartNextDirective(null)
+            break
+
+          case line instanceof Comment:
+            // this psace intentionally left blank
+            break
+
+          case line instanceof Directive:
+            // check for known metadata
+            //  title, artist...
+            switch (line.Type.toLowerCase()) {
+              case "title":
+                song.Title = line.Detail
+                break
+
+              case "artist":
+                song.Artist = line.Detail
+                break
+
+              case "key":
+                song.Key = line.Detail
+                break
+
+              case "copyright":
+                song.Copyright = line.Detail
+                break
+
+              case "license":
+                song.License = line.Detail
+                break
+
+              case "comment":
+                song.Comments.push(line.Detail)
+                break
+
+              default:
+                if (lines[0] instanceof Sentence) {
+                  line = new Lyric(line.Type)
+                }
+                StartNextDirective(line)
+                break
+            }
+            break
+
+          case line instanceof Sentence:
+            if (!currentDirective || !(currentDirective instanceof Lyric)) {
+              StartNextDirective(new Lyric("Verse"))
+            }
+            currentDirective.Sentences.push(line)
+            break
+        }
+      }
+
+      StartNextDirective()
+      return song
+    }
+
+    this.Parse = parse
+  }
+}
+
+function isLyric(l) {
+	if (l instanceof Lyric) return true
+	return !!(l.Sentences  && l.Sentences.length)
+}
+
+class ChordPro {
+  constructor() {
+    this.ParseSong = function(songText) {
+      return new SongParser(songText).Parse()
+    }
+  }
+
+  ParseFile() {}
+}
+
+export default SongParser
+export { ChordPro, SongParser, Lyric, isLyric }
