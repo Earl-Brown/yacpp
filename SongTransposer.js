136bdc6f59289837af916838161f4ded4deb45a9
Earl Brown <github@theGleep.com>
2018-11-29 07:32:25 -0700

    ES2017 and compatibility rewrite

---
+++ SongTransposer.js
@@ -0,0 +1,72 @@
+import { parse, transpose, prettyPrint } from "./libraries/chord-magic/chord-magic.js"
+import { Lyric, isLyric } from './components/parser'
+import { Sentence } from "./LeadsheetRenderer";
+
+const CONSTANTS = {
+	Sharps: ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"],
+	Flats: ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"]
+}
+function transposeChordSymbol(symbol, numberOfSemitones, options = {}) {
+	if (!symbol) return symbol		
+	
+	let parsed = parse(symbol)
+	let transposed = transpose(parsed, Number(numberOfSemitones))
+	let output = prettyPrint(transposed, options.prettyPrint || {})
+
+	return output
+}
+
+function transposeChord(c, numberOfSemitones, options) {
+	let output = {...c, Symbol: transposeChordSymbol(c.Symbol, numberOfSemitones, options)}
+	return output
+}
+
+function transposePhrase(p, numberOfSemitones, options) {
+	let output = {...p, Chord: transposeChord(p.Chord, numberOfSemitones, options) }
+	return output
+}
+
+function transposeSentence(s, numberOfSemitones, options) {
+	let output = {...s, Phrases: s.Phrases.map(p => transposePhrase(p, numberOfSemitones, options))}
+
+	return output
+}
+
+function transposeLyric(l, numberOfSemitones, options) {
+	let output = {...l, Sentences: l.Sentences.map(s => transposeSentence(s, numberOfSemitones, options))}
+	return output
+}
+
+function transposeDirective(d, numberOfSemitones, options) {
+	if (isLyric(d)) return transposeLyric(d, numberOfSemitones, options)
+
+	return d
+}
+
+class SongTransposer {
+	CONSTANTS = CONSTANTS
+  constructor(song, configuration = {}) {
+		this.Song = song
+		this.Configuration = this.setConfiguration(configuration)
+	}
+
+	setConfiguration(config) {
+		this.configuration = {
+			trasnposition: {},
+			prettyPrint: {naming: (CONSTANTS[config.SemitoneType] || undefined)}
+		}
+
+	}
+	
+	Transpose(numberOfSemitones) {
+		let {Song: song} = this
+
+		let output = {...song,
+			Key: (song.Key ? transposeChordSymbol(song.Key, numberOfSemitones, this.configuration) : ""), 
+			Directives: song.Directives.map(d => transposeDirective(d, numberOfSemitones, this.configuration))
+		}
+		return output
+	}
+}
+
+export default SongTransposer
